36f7a50ddd08a29abf0d116ee5c68d403b46944f
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-20 18:07:12 -0400

    added to function generate markdown and worked on renderLicenseLink

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) { 
-  if (license === "none"){
+  if (license === "None"){
   return ""
   }
   else {
@@ -13,18 +13,24 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (license === "None"){
+    return ""
+    }
+    else {
+      return ` * [License](#license)`
+    }
 
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === "none"){
+  if (license === "None"){
     return ""
     }
     else {
       return ` ## License
-      ${data.license}`
+      ${license}`
     }
   
  
@@ -34,6 +40,26 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
+## Description 
+${data.description}
+## Table of Contents 
+* [Usage](#usage)
+* [Credits](#credits)
+${renderLicenseLink()}
+* [Features](#features)
+* [Contributions](#contributions)
+* [Tests](#tests)
+## Usage 
+${data.usage}
+## Credits 
+${data.credits}
+${renderLicenseSection(data.license)}
+##  Features
+${data.features}
+## Contributions
+${data.howToContribute}
+## Tests
+${data.tests}
 `;
 }
 
